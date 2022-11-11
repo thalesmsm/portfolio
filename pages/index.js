9d33b9b29b33916cd623f43f01901e1fce5c88df
Thales Maciel <thalesmsm12@gmail.com>
2022-11-11 09:32:17 -0300

    add secao de navegacao

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import {BsFillMoonStarsFill } from 'react-icons/bs';
+import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
 
 export default function Home() {
   return (
@@ -9,7 +11,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className='bg-white px-10'>
+        <section className='min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl font-burtons'>Hello</h1>
+            <ul className='flex items-center'>
+              <li>
+                <BsFillMoonStarsFill className='cursor-pointer text-2xl'/>
+              </li>
+              <li>
+                <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Curriculo</a>
+              </li>
+            </ul>
+          </nav>
+        </section>
       </main>
     </div>
   )
