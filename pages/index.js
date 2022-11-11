590e0d4c6e907028ef9a2edc33bee42f3bb4ded7
Thales Maciel <thalesmsm12@gmail.com>
2022-11-11 11:11:00 -0300

    muda alguns estilos

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
-import {BsFillMoonStarsFill } from 'react-icons/bs';
+import {BsFillMoonStarsFill, BsSunFill } from 'react-icons/bs';
 import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
 import Image from 'next/image';
-import deved from '../public/dev-ed-wave.png';
+import deved from '../public/perfil.jpg';
 import code from "../public/code.png";
 import web1 from "../public/web1.png";
 import web2 from "../public/web2.png";
@@ -19,45 +19,54 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='bg-white px-10  md:px-20 lg:px-40 dark:bg-gray-900'>
-        <section className='min-h-screen'>
-          <nav className='py-10 mb-12 flex justify-between'>
+      <main className='bg-gray-50 px-10  md:px-20 lg:px-40 dark:bg-gray-900'>
+        <section className='min-h-screen lg:flex lg:flex-col lg:justify-between'>
+          <nav className='py-5 mb-12 flex justify-between'>
             <h1 className='text-xl font-burtons dark:text-gray-200'>Hello</h1>
             <ul className='flex items-center'>
               <li>
+                {!darkMode ? 
                 <BsFillMoonStarsFill 
-                onClick={() => setDarkMode(!darkMode)}
+                onClick={() => setDarkMode(true)}
                 className='cursor-pointer text-2xl'
+                /> :
+                <BsSunFill 
+                onClick={() => setDarkMode(false)}
+                className='cursor-pointer text-2xl text-yellow-300'
                 />
+                }
               </li>
               <li>
                 <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Curriculo</a>
               </li>
             </ul>
           </nav>
-          <div className='text-center p-10'>
-            <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>
-              Thales Maciel
-            </h2>
-            <h3 className='text-2xl py-2 md:text-3xl dark:text-gray-200'>
-              Desenvolvedor Fullstack
-            </h3>
-            <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-gray-200'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam excepturi, fuga hic, distinctio illo nihil quasi voluptate molestiae ipsum recusandae ratione reprehenderit laborum ut consequatur natus necessitatibus. Fuga, cum eos.
-            </p>
+          <div className='text-center p-5 lg:flex lg:items-center lg:justify-between'>
+            <div className='lg:w-1/2'>
+              <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>
+                Thales Maciel
+              </h2>
+              <h3 className='text-2xl py-2 md:text-3xl dark:text-gray-200'>
+                Desenvolvedor Fullstack
+              </h3>
+              <p className='text-md text-justify py-5 leading-8 text-gray-800 md:text-xl max-w-3xl mx-auto dark:text-gray-200'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam excepturi, fuga hic, distinctio illo nihil quasi voluptate molestiae ipsum recusandae ratione reprehenderit laborum ut consequatur natus necessitatibus. Fuga, cum eos.
+              </p>
+              <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-200'>
+                <AiFillLinkedin />
+                <AiFillGithub />
+              </div>
+            </div>
+            <div className='relative mx-auto rounded-full w-80 h-80 overflow-hidden md:h-80 md:w-80'>
+              <Image src={deved} alt='deved' layout='fill' objectFit='cover'/>
+            </div>
           </div>
-          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-200'>
-            <AiFillLinkedin />
-            <AiFillGithub />
-          </div>
-          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
-            <Image src={deved} alt='deved' layout='fill' objectFit='cover'/>
-          </div>
+          <div className='w-16'></div>
         </section>
 
-        <section>
+        <section className='min-h-screen'>
           <div>
-            <h3 className='text-3xl py-1'>
+            <h3 className='text-3xl py-1 dark:text-gray-200'>
               Services I offer
             </h3>
             <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
@@ -79,7 +88,7 @@ export default function Home() {
             {/* CRIAR COMPONENTE PARA CARDS */}
 
 
-            <div className='flex flex-col items-center text-center shadow-lg p-10 rounded-xl my-10 dark:text-gray-200'>
+            <div className='flex flex-col items-center text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-200'>
               <Image src={code} alt='design' width={100} height={100}/>
               <h3 className='text-lg font-medium pt-8 pb-2'>JavaScript</h3>
               <p className='py-2'>
@@ -91,7 +100,7 @@ export default function Home() {
               <p className='text-gray-800 py-1 dark:text-gray-200'>Node.js</p>
             </div>
 
-            <div className='flex flex-col items-center text-center shadow-lg p-10 rounded-xl my-10 dark:text-gray-200'>
+            <div className='flex flex-col items-center text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-200'>
               <Image src={code} alt='design' width={100} height={100}/>
               <h3 className='text-lg font-medium pt-8 pb-2'>JavaScript</h3>
               <p className='py-2'>
@@ -103,7 +112,7 @@ export default function Home() {
               <p className='text-gray-800 py-1 dark:text-gray-200'>Node.js</p>
             </div>
 
-            <div className='flex flex-col items-center text-center shadow-lg p-10 rounded-xl my-10 dark:text-gray-200'>
+            <div className='flex flex-col items-center text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-200'>
               <Image src={code} alt='design' width={100} height={100}/>
               <h3 className='text-lg font-medium pt-8 pb-2'>JavaScript</h3>
               <p className='py-2'>
@@ -115,7 +124,7 @@ export default function Home() {
               <p className='text-gray-800 py-1 dark:text-gray-200'>Node.js</p>
             </div>
 
-            <div className='flex flex-col items-center text-center shadow-lg p-10 rounded-xl my-10 dark:text-gray-200'>
+            <div className='flex flex-col items-center text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-200'>
               <Image src={code} alt='design' width={100} height={100}/>
               <h3 className='text-lg font-medium pt-8 pb-2'>JavaScript</h3>
               <p className='py-2'>
