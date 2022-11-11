3558fd015fb32e0e52afaa296b8697c859f312f1
Thales Maciel <thalesmsm12@gmail.com>
2022-11-11 09:47:41 -0300

    add secao para servicos e conhecimentos

--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,26 @@ export default function Home() {
             <Image src={deved} alt='deved' layout='fill' objectFit='cover'/>
           </div>
         </section>
+
+        <section>
+          <div>
+            <h3 className='text-3xl py-1'>
+              Services I offer
+            </h3>
+            <p className='text-md py-2 leading-8 text-gray-800'>
+              Since the beginning of my journey as a freelance designer and 
+              developer, I&apos;ve done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
+            </p>
+          </div>
+        </section>
       </main>
     </div>
   )
