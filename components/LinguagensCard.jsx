d9324ef247c04ed8ce08881247daed1480fb71c2
Thales Maciel <thalesmsm12@gmail.com>
2022-11-12 15:59:39 -0300

    add tecnologias

--- components/LinguagensCard.jsx
+++ components/LinguagensCard.jsx
@@ -1,17 +1,10 @@
 import Image from "next/image";
 
-function Card({src}) {
+function Card({src, name}) {
   return ( 
-    <div className='flex flex-col items-center text-center shadow-lg p-10 rounded-xl mt-10 basis-1/4 flex-1 lg:w-64 dark:bg-gray-200'>
-      <Image src={src} alt='design' width={100} height={100}/>
-      <h3 className='text-lg font-medium pt-8 pb-2'>JavaScript</h3>
-      <p className='py-2'>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus fugit quod dolores 
-      </p>
-      <h4 className='py-4 text-teal-600'>Back end</h4>
-      <p className='text-gray-800 py-1'>Node.js</p>
-      <p className='text-gray-800 py-1'>Node.js</p>
-      <p className='text-gray-800 py-1'>Node.js</p>
+    <div className='flex flex-col items-center text-center shadow-lg p-10 rounded-xl mt-10 w-40 dark:bg-gray-200'>
+      <Image src={src} alt='' width={60} height={60}/>
+      <h3 className='text-lg font-medium pt-8 pb-2'>{name}</h3>
     </div>
   );
 }
